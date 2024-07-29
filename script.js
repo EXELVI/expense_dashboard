c3759f7786622d2f53ccdce711c4060564dc3d57
EXELVI <exelvi@outlook.com>
2024-07-29 09:43:06 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const expenseForm = document.getElementById('expense-form');
+    const expenseNameInput = document.getElementById('expense-name');
+    const expenseAmountInput = document.getElementById('expense-amount');
+    const expenseList = document.getElementById('expense-list');
+    const totalAmountSpan = document.getElementById('total-amount');
+
+});
